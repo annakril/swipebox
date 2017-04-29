c11e80183580c762a1ee429db321ee87941cb763
Anna Kril <Anna Kril>
2017-04-30 00:20:47 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+( function( $ ) {
+
+	$( '.swipebox' ).swipebox( {
+		removeBarsOnMobile : false, // false will show top bar on mobile devices
+		loopAtEnd: true // true will return to the first image after the last image is reached
+	});
+
+} )( jQuery );
